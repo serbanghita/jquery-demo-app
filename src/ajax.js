2b5232e0db5622a9775bc5e9dcc0f1fd03d5518d
Serban Ghita <serbanghita@gmail.com>
2017-12-13 09:08:53 +0200

    Fixing the test.

--- src/ajax.js
+++ src/ajax.js
@@ -3,12 +3,9 @@ $.ajax({
     url: "http://localhost/whatever",
     dataType: "text",
     data: JSON.stringify({ a: "a", b: "b" }),
-    contentType: "application/json; charset=utf-8",
-    success: function (data) {
-        console.log("Success", data);
-    },
-    error: function (textStatus, errorThrown) {
-        console.error("Error", textStatus, errorThrown)
-    }
-
+    contentType: "application/json; charset=utf-8"
+}).success(function (data) {
+    // console.log("Success", data);
+}).error(function (textStatus, errorThrown) {
+    // console.error("Error", textStatus, errorThrown)
 });
